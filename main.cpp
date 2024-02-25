d56df09ade61a68641a33674520bd58b288999bb
Ioann44 <borec-ivan@bk.ru>
2024-02-25 21:04:40 +0400

    Add main part of course work

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
+#include <chrono>
+#include <ctime>
 #include <iostream>
+#include <vector>
 
-using namespace std;
+using std::cout, std::endl, std::vector, std::rand;
 
-void main() {
-	cout << "Hello world" << endl;
+template <typename T>
+static unsigned long long calcTime(size_t size) {
+	// you may want also measure allocation time
+	// auto timeStart = std::chrono::high_resolution_clock::now();
+
+	// init vectors
+	// vectors are used because of needing really large memory allocation (arrays can't do that)
+	vector<T> a(size), b(size), c(size);
+	for (auto &vc : vector{a, b}) {
+		for (auto &vci : vc) {
+			vci = (T)rand();
+		}
+	}
+
+	// main algo with time measure
+	auto timeStart = std::chrono::high_resolution_clock::now();
+	for (size_t i = 0; i < size; i++) {
+		c[i] = a[i] + b[i];
+	}
+	auto timeEnd = std::chrono::high_resolution_clock::now();
+	return (timeEnd - timeStart).count();
+}
+
+// int main(int argc, char const *argv[]) {
+int main() {
+	std::srand((uint32_t)std::time(nullptr));
+	// result in ns, divide by 1e9 to get seconds
+	cout << (double)calcTime<int>(100'000'000) / 1e9 << endl;
+	cout << (double)calcTime<float>(100'000'000) / 1e9 << endl;
+	cout << (double)calcTime<double>(100'000'000) / 1e9 << endl;
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Course work for 8th semester on discipline "Supecomputers usage"
+It fine commented, live with it.<br>
+Will leave **readme** blank for a while.
